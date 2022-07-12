a21fdcde734421816bdc0e3ea7e9efc7b8945454
KrzysztofLang <148853@stud.prz.edu.pl>
2022-07-12 18:55:14 +0200

    Enter the funkcje

--- MGR.py
+++ MGR.py
@@ -5,8 +5,8 @@ from sympy import true
 import tensorflow as tf
 import keras
 
-##Przygotowanie modelu
-def get_basic_model():
+
+def get_basic_model():  ##Przygotowanie modelu
     model = tf.keras.Sequential([
         tf.keras.layers.Dense(10, activation='relu'),
         tf.keras.layers.Dense(10, activation='relu'),
@@ -17,40 +17,52 @@ def get_basic_model():
                 metrics=['accuracy'])
     return model
 
+def wybor_kolumny():    ##Wybranie kolumny do wypełnienia
+    
+    cols = df.columns.to_list()
+    print("Dostępne kolumny:")
+    print(cols)
+    while true:
+        col = input("Etykieta kolumny do wypełnienia (aby anulować, wpisz \"koniec\"): ")
+        if col in cols:
+            print('Wybrano poprawną kolumnę ' + col)
+            break
+        elif col == 'koniec':
+            exit()
+        else:
+            print('Nie ma takiej kolumny!')
+
+    return col
+
+def przygotowanie_danych():
+    
+    ##Zamiana typow danych na kategorie, a następnie zakodowanie jako dane numeryczne w nowym DF
+    colsObjects = df.columns[df.dtypes == "object"].tolist()
+    for col in colsObjects:
+        df[col] = df[col].astype('category')
+        dfCoded = df
+        dfCoded[col] = dfCoded[col].cat.codes
+        dfCoded.loc[dfCoded[col] == -1, col] = np.nan
+
+    ##Zamiana na float dla ujednolicenia typu
+    dfCoded = dfCoded.astype(np.float64)
+
+    ##Podzielenie Dataframe na zawierające NaN w wybranej kolumnie i wypełnione
+    dfAllNan = dfCoded[dfCoded[col].isnull()]
+    dfNoNan = dfCoded[~dfCoded[col].isnull()]
+
+    ##Wydzielenie danych uczących
+    dfNoNanTarget = dfNoNan.pop(col)
+    dfAllNanTarget = dfAllNan.pop(col)
+
+    return dfAllNan, dfNoNan, dfAllNanTarget, dfNoNanTarget
+
 ##Wczytanie pliku danych
 df = pd.read_csv("adult.data")
 
-##Zamiana typow danych na kategorie, a następnie zakodowanie jako dane numeryczne w nowym DF
-colsObjects = df.columns[df.dtypes == "object"].tolist()
-for col in colsObjects:
-    df[col] = df[col].astype('category')
-    dfCoded = df
-    dfCoded[col] = dfCoded[col].cat.codes
-    dfCoded.loc[dfCoded[col] == -1, col] = np.nan
+col = wybor_kolumny()
 
-##Zamiana na float dla ujednolicenia typu
-dfCoded = dfCoded.astype(np.float64)
-
-##Wybranie kolumny do wypełnienia
-cols = dfCoded.columns.to_list()
-print("Dostępne kolumny:")
-print(cols)
-while true:
-    col = input("Etykieta kolumny do wypełnienia (aby anulować, wpisz \"koniec\"): ")
-    if col in cols:
-        print('Wpisano poprawnie')
-        break
-    elif col == 'koniec':
-        exit()
-    else:
-        print('Nie ma takiej kolumny!')
-
-##Podzielenie Dataframe na zawierające NaN w wybranej kolumnie i wypełnione
-dfAllNan = dfCoded[dfCoded[col].isnull()]
-dfNoNan = dfCoded[~dfCoded[col].isnull()]
-
-dfNoNanTarget = dfNoNan.pop(col)
-dfAllNanTarget = dfAllNan.pop(col)
+dfAllNan, dfNoNan, dfAllNanTarget, dfNoNanTarget = przygotowanie_danych()
 
 model = get_basic_model()
 model.fit(dfNoNan, dfNoNanTarget, epochs=15, batch_size=2)
--- NaN_gen.py
+++ NaN_gen.py
@@ -1,8 +1,63 @@
+from cmath import nan
 from typing import Type
 import numpy as np
 import pandas as pd
+from sympy import true
+from random import sample
+
+def wybory():   ##Uruchamianie funkcji wyboru
+    print("Aby wyjśćz programu, wpisz \"koniec\".")
+    col = wybor_kolumny()
+    nanRate = wybor_procentow()
+
+    return col, nanRate
+
+def wybor_kolumny():    ##Wybranie kolumny do dziurawienia
+    cols = df.columns.to_list()
+    print("Dostępne kolumny:")
+    print(cols)
+    while true:
+        col = input("Etykieta kolumny usunięcia wartości: ")
+        if col in cols:
+            print('Wybrano poprawną kolumnę ' + col)
+            break
+        elif col == 'koniec':
+            exit()
+        else:
+            print('Nie ma takiej kolumny!')
+
+    return col
+
+def wybor_procentow():    ##Wybranie ile % pustych wartości ma zostać stworzone
+    while true:
+        nanRate = input("Ile % wartości ma być usuniętych?: ")
+        if nanRate == 'koniec':
+            exit()
+        else:
+            try:
+                nanRate = float(nanRate)
+                break
+            except ValueError:
+                print("Błąd wartości: proszę podać liczbę rzeczywistą, z kropką jako separator.")
+
+    return nanRate
+
+def usuwanie_wartosci(df, col, nanRate):    ##Usuwanie wybranej ilości wartości z wskazanej kolumny i zapisanie jako nowy plik
+    ind = len(df.index)
+    numToRem = int(nanRate*0.01*ind)
+    print(numToRem)
+    indToRem = sample(range(ind),numToRem)
+    indToRem.sort()
+    print(indToRem)
+    for i in indToRem:
+        df.loc[i,col] = np.nan
+    df.to_csv('test.csv', index=False)
 
 ##Wczytanie pliku danych
 df = pd.read_csv("indexData_NYA.csv")
 
 print(df.info())
+
+col, nanRate = wybory()
+
+usuwanie_wartosci(df, col, nanRate)
