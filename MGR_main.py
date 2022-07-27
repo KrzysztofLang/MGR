3a3553d7ace240bcf71ec30687470666fd2df9c6
KrzysztofLang <148853@stud.prz.edu.pl>
2022-07-27 19:35:08 +0200

    Próby przywrócenia DF po wypenieniu kolumny

--- MGR_main.py
+++ MGR_main.py
@@ -12,12 +12,33 @@ import re
 class Dane:
     def __init__(self) -> None:
 
-        self.enc = OrdinalEncoder(
+        # Przygotowanie encoderów
+        self.enc_features_all = OrdinalEncoder(
             handle_unknown="use_encoded_value", unknown_value=np.nan
         )
+
+        self.enc_features_no = OrdinalEncoder(
+            handle_unknown="use_encoded_value", unknown_value=np.nan
+        )
+
+        self.enc_target = OrdinalEncoder(
+            handle_unknown="use_encoded_value", unknown_value=np.nan
+        )
+
         # Wybranie pliku do wypełniania
         print('Aby wyjść z programu, wpisz "koniec".\n')
         self.df = self.wybor_pliku()
+
+        # Lista wszystkichh kolumn
+        self.columns = list(self.df)
+
+        # Lista kolumn z damnymi kategorycznymi
+        self.cols_with_objects = self.df.columns[
+            self.df.dtypes == "object"
+        ].tolist()
+
+        # Utworzenie listy kolumn do wypełnienia,
+        # posortowana od najmniejszej do największej ilości NaN
         self.cols_to_fill = {}
         for cols in self.df.columns[self.df.isna().any()].tolist():
             if (
@@ -31,8 +52,6 @@ class Dane:
                 sorted(self.cols_to_fill.items(), key=lambda item: item[1])
             ).keys()
         )
-        print(self.cols_to_fill)
-        exit()
 
     # Wybranie i wczytanie pliku do pracy
     @staticmethod
@@ -62,51 +81,109 @@ class Dane:
         return df
 
     # Przygotowanie danych do dalszej pracy w wypadku gdy
-    # Wybrana kolumna zawiera dane kategoryczne
+    # wybrana kolumna zawiera dane kategoryczne
     def przygotowanie_danych_kategoryczne(self):
-        # Zamiana typow danych na kategorie, a następnie zakodowanie jako dane
-        # numeryczne w nowym DF
-        cols_with_objects = self.df.columns[
-            self.df.dtypes == "object"
-        ].tolist()
 
-        # Kodowanie danych katygorycznych z uzyciem etykiet
+        # Wybranie kolumny do wypełnienia
+        self.col = self.cols_to_fill[0]
 
         # Podzielenie Dataframe na zawierające NaN w wybranej kolumnie i
         # wypełnione
         df_all_nan = self.df[self.df[self.col].isnull()]
         df_no_nan = self.df[~self.df[self.col].isnull()]
 
+        # Usinięcie rekordów z NaN dla poprawienia jakości uczenia
         df_no_nan = df_no_nan.dropna()
 
-        features_all_nan = df_all_nan.drop(self.col, axis=1)
-        target_all_nan = df_all_nan[self.col]
-        features_no_nan = df_no_nan.drop(self.col, axis=1)
-        target_no_nan = df_no_nan[self.col]
+        # Podzielenie DF na dane uczące i cele
+        self.features_all_nan = df_all_nan.drop(self.col, axis=1)
+        self.target_all_nan = df_all_nan[self.col]
+        self.features_no_nan = df_no_nan.drop(self.col, axis=1)
+        self.target_no_nan = df_no_nan[self.col]
+
+        # Utworzenie list z nazwami kolumn
+        self.features_all_nan_columns = list(self.features_all_nan)
+        self.target_all_nan_columns = list(self.target_all_nan)
+        self.features_no_nan_columns = list(self.features_no_nan)
+        self.target_no_nan_columns = list(self.target_no_nan)
 
         # Utworzenie listy z ID kolumn zawierających dane kategoryczne
-        cat_arr = []
+        self.cat_arr = []
 
-        for cols in features_all_nan:
-            if cols in cols_with_objects:
-                cat_arr.append(features_all_nan.columns.get_loc(cols))
+        for cols in self.features_all_nan:
+            if cols in self.cols_with_objects:
+                self.cat_arr.append(
+                    self.features_all_nan.columns.get_loc(cols)
+                )
 
-        return (
-            features_all_nan,
-            target_all_nan,
-            features_no_nan,
-            target_no_nan,
-            cat_arr,
+        # Konwersja DF na numpy array i kodowanie Label Encoding
+        self.features_all_nan = self.features_all_nan.to_numpy()
+        self.target_all_nan = self.target_all_nan.to_numpy()
+        self.features_no_nan = self.features_no_nan.to_numpy()
+        self.target_no_nan = self.target_no_nan.to_numpy()
+        self.features_no_nan = self.enc_features_no.fit_transform(
+            self.features_no_nan
+        )
+        self.target_no_nan = self.enc_target.fit_transform(
+            self.target_no_nan.reshape(-1, 1)
+        ).ravel()
+        self.features_all_nan = self.enc_features_all.fit_transform(
+            self.features_all_nan
+        )
+        self.target_all_nan = self.enc_target.fit_transform(
+            self.target_all_nan.reshape(-1, 1)
+        ).ravel()
+
+
+        del self.cols_to_fill[0]
+
+    def przywroc_df(self):
+
+        # Przywrócenie danym kategorycznym odpowiednich wartości
+        self.features_no_nan = self.enc_features_no.inverse_transform(
+            self.features_no_nan
+        )
+        self.target_no_nan = self.enc_target.inverse_transform(
+            self.target_no_nan.reshape(-1, 1)
+        ).ravel()
+        self.features_all_nan = self.enc_features_all.inverse_transform(
+            self.features_all_nan
+        )
+        self.target_all_nan = self.enc_target.inverse_transform(
+            self.target_all_nan.reshape(-1, 1)
+        ).ravel()
+
+
+        # Zamiana Numpy Array na DataFrame
+        self.features_all_nan = pd.DataFrame(
+            self.features_all_nan, columns=self.features_all_nan_columns
+        )
+        self.target_all_nan = pd.DataFrame(
+            self.target_all_nan, columns=self.target_all_nan_columns
+        )
+        self.features_no_nan = pd.DataFrame(
+            self.features_no_nan, columns=self.features_no_nan_columns
+        )
+        self.target_no_nan = pd.DataFrame(
+            self.target_no_nan, columns=self.target_no_nan_columns
         )
 
+        self.df_all_nan = self.features_all_nan.merge(self.target_all_nan)
+        self.df_no_nan = self.features_no_nan.merge(self.target_no_nan)
 
-def naucz_model(dane):
-    features = dane.features_no_nan.to_numpy()
-    target = dane.target_no_nan.to_numpy()
+        print(self.df_all_nan.info())
+
+
+def naucz_i_wypełnij(dane):
+
+    dane.przygotowanie_danych_kategoryczne()
 
     # Wydzielenie zbiorów uczących i testowych
     x_train, x_test, y_train, y_test = train_test_split(
-        features, target, test_size=0.3, random_state=109
+        dane.features_no_nan,
+        dane.target_no_nan,
+        test_size=0.3,
+        random_state=109,
     )
 
     # Nauka modelu
@@ -118,21 +195,11 @@ def naucz_model(dane):
     y_pred = clf.predict(x_test)
     print("Skuteczność: ", metrics.accuracy_score(y_test, y_pred))
 
-    return clf
+    dane.target_all_nan = clf.predict(dane.features_all_nan)
 
-
-def wypelnij_puste(clf, dane):
-    x_fill = dane.features_all_nan.to_numpy()
-
-    y_fill = clf.predict(x_fill)
-
-    filled = np.append(x_fill, y_fill)
-
-    print(filled)
+    dane.przywroc_df()
 
 
 dane = Dane()
 
-clf = naucz_model(dane)
-
-wypelnij_puste(clf, dane)
+naucz_i_wypełnij(dane)
